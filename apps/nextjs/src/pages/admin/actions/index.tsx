0b2ebb1ee398abf3f0c1a13e81f84743d4f915d0
Troy Underwood <troyizzle@gmail.com>
2023-07-06 14:14:35 -0500

    shadcn

--- apps/nextjs/src/pages/admin/actions/index.tsx
+++ apps/nextjs/src/pages/admin/actions/index.tsx
@@ -1,6 +1,12 @@
+import { Button } from "../../../../@/components/ui/button";
 import { trpc } from "../../../utils/trpc"
 
 export default function Page() {
   const babyQuery = trpc.baby.all.useQuery();
-  return <div>Hello</div>
+
+  return (
+    <div>
+      <Button variant="destructive">HELLO</Button>
+    </div>
+  )
 }
