1409701c2506d310bde6faf0c7e4d2a017a70b2e
Troy Underwood <troyizzle@gmail.com>
2023-07-06 14:16:47 -0500

    refactor to under src

--- apps/nextjs/src/pages/admin/actions/index.tsx
+++ apps/nextjs/src/pages/admin/actions/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../../../../@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { trpc } from "../../../utils/trpc"
 
 export default function Page() {
