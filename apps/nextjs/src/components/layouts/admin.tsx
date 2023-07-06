082d6c7816623e9927e0be9a05803dfe2233313a
Troy Underwood <troyizzle@gmail.com>
2023-07-06 17:42:40 -0500

    fix admin alyout

--- apps/nextjs/src/components/layouts/admin.tsx
+++ apps/nextjs/src/components/layouts/admin.tsx
@@ -55,7 +55,7 @@ export default function AdminLayout({ title, children, createForm }: AdminLayout
       <Navbar />
       <div className="space-y-6 p-10 pb-16">
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/2">
+          <aside className="-mx-4 lg:w-1/6">
             <SidebarNavMenu />
           </aside>
           <div className="flex-1">
