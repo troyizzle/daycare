72ab88a034728d10dbaa4d98c7ac7c63d36b2e00
Troy Underwood <troyizzle@gmail.com>
2023-08-05 14:08:19 -0500

    feat(mobile): change splash picture

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -7,12 +7,11 @@ const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   slug: "daycare",
   version: "1.0.0",
   orientation: "portrait",
-  icon: "./assets/icon.png",
+  icon: "./assets/splash.png",
   userInterfaceStyle: "light",
   splash: {
-    image: "./assets/icon.png",
+    image: "./assets/splash.png",
     resizeMode: "contain",
-    backgroundColor: "#2e026d",
   },
   updates: {
     fallbackToCacheTimeout: 0,
@@ -24,8 +23,7 @@ const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: "./assets/icon.png",
-      backgroundColor: "#2e026d",
+      foregroundImage: "./assets/splash.png",
     },
   },
   extra: {
