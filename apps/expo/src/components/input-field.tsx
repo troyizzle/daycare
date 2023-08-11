929da7958678c69e5a5dafe7380a6e08f0440e1d
Troy Underwood <troyizzle@gmail.com>
2023-08-10 21:14:26 -0500

    feat(mobile): refactor input field

--- apps/expo/src/components/input-field.tsx
+++ apps/expo/src/components/input-field.tsx
@@ -1,13 +1,23 @@
+import { useTheme } from "@react-navigation/native"
 import { TextInput, View } from "react-native"
 
 type InputFieldProps = React.ComponentProps<typeof TextInput>
 
 export default function InputField({ ...props }: InputFieldProps) {
+  const { colors } = useTheme()
+
   return (
     <View
-      className="flex p-4 rounded border border-gray-300 shadow"
+      className="flex p-4 rounded border shadow"
+      style={{
+        borderColor: colors.border,
+      }}
     >
       <TextInput
+        style={{
+          color: colors.text,
+        }}
+        placeholderTextColor={colors.text}
         {...props}
       />
     </View>
