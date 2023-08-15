b98bcc34cb7cf8b8144b2a04e2aa83ad1c0912da
Troy Underwood <troyizzle@gmail.com>
2023-08-15 14:24:27 -0500

    add back app

--- apps/expo/src/app.tsx
+++ apps/expo/src/app.tsx
@@ -11,8 +11,6 @@ import Background from "./components/background";
 import ColorSchemeProvider from "./contexts/color-scheme-provider";
 import { StatusBar } from "react-native";
 
-
-
 export const App = () => {
   const theme = createTheme();
 
@@ -20,7 +18,23 @@ export const App = () => {
     <NavigationContainer>
       <ThemeProvider theme={theme}>
         <ColorSchemeProvider>
-          <Text>Hello</Text>
+          <Background>
+            <ClerkProvider
+              publishableKey="pk_test_d29uZHJvdXMtY293YmlyZC04LmNsZXJrLmFjY291bnRzLmRldiQ"
+              tokenCache={tokenCache}
+            >
+              <SignedIn>
+                <TRPCProvider>
+                  <PushNotificationProvider>
+                    <Navigation />
+                  </PushNotificationProvider>
+                </TRPCProvider>
+              </SignedIn>
+              <SignedOut>
+                <SigninNavigation />
+              </SignedOut>
+            </ClerkProvider>
+          </Background>
           <StatusBar />
         </ColorSchemeProvider>
       </ThemeProvider>
