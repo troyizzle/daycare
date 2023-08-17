56cbee43c65b891d32778902c9d93b003d455665
Troy Underwood <troyizzle@gmail.com>
2023-08-17 18:13:49 -0500

    set debug

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -24,6 +24,7 @@
 import { authMiddleware } from "@clerk/nextjs"
 
 export default authMiddleware({
+  debug: true,
   afterAuth: async (auth) => {
     console.log("after auth", auth)
     console.log("aouth token", await auth.getToken())
